0e3e891d1d3db15c860548537746c393099d921f
thientrankhh <thientrankhh@gmail.com>
2020-03-24 00:19:08 +0700

    Exercise 3

--- js.js
+++ js.js
@@ -20,33 +20,42 @@ function student(fn, ln, gd, bd, lop, khoa, nganh, diem1, diem2, diem3) {
 	this.diem1 = diem1;
 	this.diem2 = diem2;
 	this.diem3 = diem3;
-	this getDiemtrungbinh = function() {
-		return (this.diem1 + this.diem2 + this.diem3);
+	this.getDiemtrungbinh = function() {
+		return (parseFloat(this.diem1) + parseFloat(this.diem2) + parseFloat(this.diem3))/3;
 	}
-	this xeploai = function() {
+	this.xeploai = function() {
 		if (this.getDiemtrungbinh() >= 9 && this.getDiemtrungbinh() <= 10) {
 			return 'xếp loại Xuất Sắc';
-		} elseif (this.getDiemtrungbinh >= 8 && this.getDiemtrungbinh < 9) {
+		} else if (this.getDiemtrungbinh() >= 8 && this.getDiemtrungbinh() < 9) {
 			return 'xếp loại Gioi';
-		} elseif (this.getDiemtrungbinh >=7 && this.getDiemtrungbinh < 8) {
+		}
+		else if (this.getDiemtrungbinh() >=7 && this.getDiemtrungbinh() < 8) {
 			return 'xep loai Kha';
-		} elseif (this.getDiemtrungbinh>= 5 && this.getDiemtrungbinh < 7) {
-			return 'xep laoi Trung Binh';
-		} else {
+		}
+		else if (this.getDiemtrungbinh() >= 5 && this.getDiemtrungbinh() < 7) {
+			return 'xep loai Trung Binh';
+		}
+		else {
 			return 'xep loai Yeu';
 		}
+		
 	}
+
 	this.studentInfo = function () {
 		return 'Name' + (this.fn + ' ' + this.ln) + '<br>Gender: ' + this.gd + '<br>Birthday: ' + this.bd + '<br>Lớp: ' + 
 		this.lop + '<br>Khoa: ' + this.khoa + '<br>Điểm trung bình: ' + this.getDiemtrungbinh() + '<br>Xếp loại: ' + this.xeploai();
 	}
 }
-
+student.prototype.thongtindiem = function () {
+	return '-Điểm môn 1: ' + this.diem1 + '	-Điểm môn 2: ' + this.diem2 + '	-Điểm môn 3: ' + this.diem3 + ' -Điểm trung bình: ' + this.getDiemtrungbinh();
+}
 
 var person1 = new person("Trần", "Thiên", "Nam", "06/06/1995");
 console.log(person1.fullname());
 console.log(person1.personInfo());
 
-var student1 = new student('Trần', "Thiên", "Nam", "06/06/1995", "01", "Vl", 6, 7, 8); 
+var student1 = new student('Trần', "Thiên", "Nam", "06/06/1995", "01", "Vat Ly","Vat Ly", 6, 7, 8); 
+console.log(student1.getDiemtrungbinh());
+console.log(student1.thongtindiem());
 console.log(student1.studentInfo());
 
